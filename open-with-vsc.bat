fd5b969fbb34e50bef22777b095c27bebf4b6c26
WillDone-MT <wildanmufti350@gmail.com>
2025-11-12 21:28:33 +0700

    Update .bat to v0.0.3 running te .bat will open the root folder and instantly focus into the README file

--- open-with-vsc.bat
+++ open-with-vsc.bat
@@ -1,4 +1,6 @@
 @echo off
-REM Open the folder where this script is located
+REM v0.0.3
 REM Please be careful when opening files from the internet! this .bat file especially just to ease (myself) for opening the folder
-code "%~dp0"
+REM Open the folder where this script is located
+set "root=%~dp0"
+code "%root:~0,-1%" --new-window "%root:~0,-1%\README.md"
