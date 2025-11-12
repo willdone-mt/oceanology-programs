b270ebae99d62797dc80de8c3d93bf5ed6bf2612
WillDone-MT <wildanmufti350@gmail.com>
2025-11-12 18:51:02 +0700

    Added a .bat file

---
+++ open-with-vsc.bat
@@ -0,0 +1,4 @@
+@echo off
+REM Open the folder where this script is located
+REM Please be careful when opening files from the internet! this .bat file especially just to ease (myself) for opening the folder
+code "%~dp0"
